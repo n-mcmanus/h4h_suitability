30bdfcba28c542694fd22b3a9f7be6d5c91c0a1e
n-mcmanus <nmcmanus1206@gmail.com>
2025-01-22 14:42:45 -0800

    initial commit

---
+++ scripts/0_prep_data.R
@@ -0,0 +1,19 @@
+# This script wrangles multiple data used in this analysis
+
+## Read in packages
+library(tidyverse)            # always
+library(terra)                # GIS functions
+library(sf)                   # vector functions
+library(rnaturalearth)        # get countries oulintes
+library(rnaturalearthdata)    # ' '
+
+# ROI ------------------------------------------------------------------
+## First generate shapefile defining region of interest to crop
+## and mask all other data
+
+
+
+# NatureBase
+# Direct download from: https://app.naturebase.org/data (all pathways)
+
+## 
